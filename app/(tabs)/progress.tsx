75750d338bf5e62d887cd798d36f121f29122d16
Sapat Chaudhary <chy.sapat@gmail.com>
2026-05-23 15:21:50 +0545

    Create learning screen basic ui

--- app/(tabs)/progress.tsx
+++ app/(tabs)/progress.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Progress = () => {
   return (
-    <View>
-      <Text>Progress</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-slate-50 dark:bg-slate-900 pt-4">
+      <View className="items-center justify-center">
+        <Text className="text-4xl font-nunito-bold text-slate-600 dark:text-slate-300">
+          Your Progress
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
